85b971553b1db4a4a6f60621b3760f454b87679c
Saad Hasan <saadh393@gmail.com>
2022-09-08 16:24:58 +0600

    ad

--- src/Util.js
+++ src/Util.js
@@ -1,4 +1,4 @@
-export const DOMAIN = "https://strapi-risalat-siddik.herokuapp.com/";
+export const DOMAIN = "https://strapi-risalat-siddik.herokuapp.com";
 export const AUTH =
   "Bearer 7a5bbf27ae84d55dfaf1423cc8e7aa1d5809680bdb2cad59f38658f9d10d2277e2c03f42fbd59ddd390ec6944242e92ea6609b9915ccecdfd820ce885e5fc2e0d9266f639ae7578648592386c5f56b1db56fa49562b339b3c000df7ea3854446441e977c160ca90ca9b12cc3e62c0963d7a7feeb70941f37a62766a254cbfd79";
 
